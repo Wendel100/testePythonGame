df441aa5f2c5162fe892701448c45b56d6acc547
Wendel <renanwendel100@gmail.com>
2025-04-03 00:04:51 -0300

    version 1.0.4

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ WINDOW_TITLE = "Jogo de Plataforma"
 JOGADOR_VELOCIDADE = 3
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 16
-
+COIN_SCALING = 1.4
 class GameView(arcade.Window):
     
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
         self.player_texture = None
         self.player_sprite = None
         self.player_list = None
         self.wall_list = None
+        self.camera = None
+        self.coin_list = None
+        
     def setup(self):
+         
          self.player_texture = arcade.load_texture("Images\Personagen.png")
          self.player_sprite = arcade.Sprite(self.player_texture)
          self.player_sprite.center_x = 100
@@ -23,27 +28,45 @@ class GameView(arcade.Window):
          self.player_list = arcade.SpriteList()
          self.player_list.append(self.player_sprite)
          self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+         self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+
          for x in range(0, 1250, 64):
             wall = arcade.Sprite("Images\Terreno.png", scale=0.5)
             wall.center_x = x
             wall.center_y = 32
             self.wall_list.append(wall)
             coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+            for x in range(128, 1250, 256):
+                coin = arcade.Sprite("Images\Moeda.png", scale=COIN_SCALING)
+                coin.center_x = x
+                coin.center_y = 96
+                self.coin_list.append(coin)
+
             for coordinate in coordinate_list:
                  wall = arcade.Sprite("Images\Caixa.png", scale=0.2)
                  wall.position = coordinate
                  self.wall_list.append(wall)
                  self.background_color = arcade.csscolor.BLUE_VIOLET
                  self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, walls=self.wall_list, gravity_constant=GRAVITY)
+                 self.camera = arcade.Camera2D()
+                 
     def on_draw(self):
+             
              self.clear()
+             self.camera.use()
              arcade.draw_sprite(self.player_sprite)
              self.player_list.draw()
              self.wall_list.draw()
+             self.coin_list.draw()
 
     def on_update(self, delta_time):
 
         self.physics_engine.update()
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
+        self.camera.position = self.player_sprite.position
 
     def on_key_press(self, key, modifiers):
         
